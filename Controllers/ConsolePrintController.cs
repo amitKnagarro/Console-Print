8339b9956ddc801ca1d402b023f5b420cf8e41cd
amitkhare <amit.khare@nagarro.com>
2024-04-08 18:04:07 +0530

    Add project files.

---
+++ Controllers/ConsolePrintController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Console_Print.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsolePrintController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok(new { Message = "Hello World" });
+        }
+    }
+}
